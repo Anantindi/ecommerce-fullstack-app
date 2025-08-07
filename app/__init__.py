81bc86eb8456b9df5e56c3208392f180b6be43b6
Anantindi <111069093+Anantindi@users.noreply.github.com>
2025-08-07 13:43:36 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,16 +10,16 @@ mail = Mail()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = '56e0968a5b5b532f19c871f9a7713cb0a5e1dcf17d1c3d5fcdf8e70486ff8826'
+    app.config['SECRET_KEY'] = '56e0968a5b5b532f19c871f9a7713cb0a5e1dcf17d1c3d5fcdf8e70486ff8826' #use your sceret key
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
 
     # Flask-Mail config
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'hulkandtheagentsofsmash2@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'tmzkiiejjvwjtvvm'
-    app.config['MAIL_DEFAULT_SENDER'] = 'hulkandtheagentsofsmash2@gmail.com'
+    app.config['MAIL_USERNAME'] = '  ' #your email id here
+    app.config['MAIL_PASSWORD'] = "   "#your email password here
+    app.config['MAIL_DEFAULT_SENDER'] = ' '#your email id here
 
     db.init_app(app)
     login_manager.init_app(app)
